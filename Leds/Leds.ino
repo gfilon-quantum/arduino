1d3b75f3641b169a55d336c307fbf531c304fee8
Gonzalo Filón <Gonzalo Filn>
2016-08-01 21:47:23 -0300

    Prueba

---
+++ Leds/Leds.ino
@@ -0,0 +1,10 @@
+#define ROJO 13
+void setup() {
+  // put your setup code here, to run once:
+  pinMode (ROJO, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(ROJO, HIGH);
+}
